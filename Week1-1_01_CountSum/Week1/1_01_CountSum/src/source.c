2af6267981449b46b910c67eee928f8797fc0784
bwdq <louis.marquiss@seattlecolleges.edu>
2024-08-10 22:27:26 -0700

    update tmc

--- Week1-1_01_CountSum/Week1/1_01_CountSum/src/source.c
+++ Week1-1_01_CountSum/Week1/1_01_CountSum/src/source.c
@@ -2,5 +2,8 @@
 #include "source.h"
 
 void count_sum(void) {
-    
+    // take two integers from the user
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    printf("%d + %d = %d\n", num1, num2, num1 + num2);
 }
